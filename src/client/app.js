9429a5a57a1961df11e451d9f3ab8f8b4358cdbb
Nick Karnik <theoutlander@live.com>
2017-12-15 15:21:01 -0800

    Integrated enzyme

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends Component {
+export default class App extends Component {
   render () {
     return <div>Welcome to React Boilerplate App</div>
   }
 }
-
-ReactDOM.render(<App />, document.getElementById('root'))
